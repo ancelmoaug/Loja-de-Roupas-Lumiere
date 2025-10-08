72c09b16c054362764c6b081aadbef39de4c794a
Ancelmo <ancelmoaugusto15@gmail.com>
2025-10-08 08:06:12 -0300

    Ajustando pastas.

---
+++ src/main/java/App.java
@@ -0,0 +1,8 @@
+import model.Fornecedor;
+
+public class App {
+    public static void main(String[] args) {
+        Fornecedor f = new Fornecedor(null, null, null, null, null, null);
+        System.out.println(f.toString());
+    }
+}
