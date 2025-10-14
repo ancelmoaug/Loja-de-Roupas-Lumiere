46b5ea12212583fb5b3a57d32e0659f25816736b
curso <curso@ifba.irece.edu.br>
2025-10-14 11:32:40 -0300

    Alterações de Júlia Brabosa

--- src/main/java/service/CategoriaService.java
+++ src/main/java/service/CategoriaService.java
@@ -1,19 +1,32 @@
 package service;
 
+import db.DB;
+import impl.CategoriaDAOImpl;
+import model.Categoria;
+
 public class CategoriaService {
-    /*
+
     private CategoriaDAOImpl categoriaDAOImpl;
 
     public CategoriaService() {
         categoriaDAOImpl = new CategoriaDAOImpl(DB.getConnection());
-    } 
-
-    Categoria buscarPorId(int id) {
-    // código 
     }
 
-    int buscarPorCategoria(String nomeCategoria) {
-    // código
+    public Categoria buscarPorId(int id) {
+        if (id <= 0) {
+            System.out.println("ID inválido!");
+            return null;
+        }
+        return categoriaDAOImpl.buscarPorId(id);
     }
-    */
+
+    public int buscarPorCategoria(String nomeCategoria) {
+        if (nomeCategoria == null || nomeCategoria.trim().isEmpty()) {
+            System.out.println("O nome da categoria não pode ser vazio.");
+            return -1;
+        }
+        return categoriaDAOImpl.buscarPorCategoria(nomeCategoria);
+    }
 }
+
+   
